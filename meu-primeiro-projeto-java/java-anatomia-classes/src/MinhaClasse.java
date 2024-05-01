e1382fa368c7d87469fcca291e27ef96302221b7
Jayme Alves <jaymealves123@hotmail.com>
2024-05-01 20:04:12 -0300

    Passando parametros

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primenoNome = "Jam";
+        String segundoNome = "Alves";
+        
+        String nomeCompleto = nomeCompleto(primenoNome, segundoNome);
+        System.out.println(nomeCompleto);
+}
+
+public static String nomeCompleto (String primeiroNome, String segundoNome){
+    return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+}
+
+}
