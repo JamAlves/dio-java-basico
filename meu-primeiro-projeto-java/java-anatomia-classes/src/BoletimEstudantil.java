30776eea3c03b3358b866599d65a8970850b2025
Jayme Alves <jaymealves123@hotmail.com>
2024-05-01 22:22:49 -0300

    identaçãoCode

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/BoletimEstudantil.java
@@ -0,0 +1,13 @@
+public class BoletimEstudantil {
+    public static void main(String[] args) {
+        int mediaFinal = 6;
+            if (mediaFinal < 6) {
+                System.out.println("Reprovado");
+             if (mediaFinal == 6)
+                System.out.println("Prova minerva");
+            else
+                System.out.println("Reprovado"); 
+            
+    }
+ }
+}
