e3cbcdb6228c727f12c622dcbc8b29eeac98599f
Jayme Alves <jaymealves123@hotmail.com>
2024-05-06 14:02:52 -0300

    Tiposvariavel1

--- meu-primeiro-projeto-java/java-anatomia-classes/src/Tiposvariaveis.java
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/Tiposvariaveis.java
@@ -4,9 +4,18 @@ public class Tiposvariaveis {
      * @param args
      */
     public static void main(String[] args) {
+        String meuNome = "Jam Alves";
         double salarioMinimo = 2500;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
-    }
+
+        int numero = 5;
+         numero = 10;
+        System.out.println(numero);  
+        
+        final double Valor_de_PI = 3.14;
+
+        System.out.println(Valor_de_PI);
+}
 }
