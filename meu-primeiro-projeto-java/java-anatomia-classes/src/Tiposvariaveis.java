e26e9c437b498e6191f34562055c8d9fd44a034a
Jayme Alves <jaymealves123@hotmail.com>
2024-05-06 13:53:23 -0300

    Tipos de variavel

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/Tiposvariaveis.java
@@ -0,0 +1,12 @@
+
+public class Tiposvariaveis {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        double salarioMinimo = 2500;
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+    }
+}
