99b74d619e409ce2457a9d22b1c1b015112d1829
Jayme Alves <jaymealves123@hotmail.com>
2024-05-06 18:19:22 -0300

    OperadoresDeAtribuição

---
+++ meu-primeiro-projeto-java/java-anatomia-classes/src/Operadores.java
@@ -0,0 +1,13 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem" + "Java";
+        System.out.println(nomeCompleto);
+
+        int numero = 5;
+        //numero++;
+        System.out.println(numero --);
+
+        System.out.println(numero);
+
+    }
+}
